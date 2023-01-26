ae44e93932c341fa0124fd26d8363a7edf11e7b2
Steph Hjartarson <stephanie.hja@gmail.com>
2023-01-26 22:16:53 +0000

    Reflect currently logged in user's state

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,5 +1,4 @@
 import axios from "axios";
-import { AccordionCollapse } from "react-bootstrap";
 
 axios.defaults.baseURL = "https://steph-drf-api.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
